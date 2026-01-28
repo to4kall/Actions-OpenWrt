926e38afaee1f1542a2e279b98bcc868d59d5b7c
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-01-28 08:47:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,7 +29,7 @@ git clone https://github.com/lkiuyu/DbusSmsForwardCPlus package/DbusSmsForwardCP
 curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
 
 # mode
-echo 'src-git modem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+echo 'src-git QModem https://github.com/FUjr/QModem' >> feeds.conf.default
 
 # OpenClash
 git clone --depth 1 https://github.com/vernesong/OpenClash.git OpenClash
